0fdc71311000c41fb751ae40271e3718527742d8
Balaji <balaji.sampath@lexisnexis.com>
2017-01-30 17:09:04 -0500

    Fixed URL encoding reserved words  bug

--- Source/R/hpccConnect.R
+++ Source/R/hpccConnect.R
@@ -16,6 +16,24 @@ r2hpcc.hpccConnect<- function (host, targetCluster,userId, password,  resultLimi
 	hpcctargetCluster <- targetCluster
 	hpccuserid <- userId
 	hpccpassword <- password  
+	hpccpassword <- gsub("\\!","%21",password )
+	hpccpassword <- gsub("\\#","%23",hpccpassword )
+	hpccpassword <- gsub("\\$","%24",hpccpassword )
+	hpccpassword <- gsub("\\&","%26",hpccpassword )
+	hpccpassword <- gsub("\\'","%27",hpccpassword )
+	hpccpassword <- gsub("\\(","%28",hpccpassword )
+	hpccpassword <- gsub("\\)","%29",hpccpassword )
+	hpccpassword <- gsub("\\*","%2A",hpccpassword )
+	hpccpassword <- gsub("\\+","%2B",hpccpassword )
+	hpccpassword <- gsub("\\,","%2C",hpccpassword )
+	hpccpassword <- gsub("\\/","%2F",hpccpassword )
+	hpccpassword <- gsub("\\:","%3A",hpccpassword )
+	hpccpassword <- gsub("\\;","%3B",hpccpassword )
+	hpccpassword <- gsub("\\=","%3D",hpccpassword )
+	hpccpassword <- gsub("\\?","%3F",hpccpassword )
+	hpccpassword <- gsub("\\@","%40",hpccpassword )
+	hpccpassword <- gsub("\\[","%5B",hpccpassword )
+	hpccpassword <- gsub("\\]","%5D",hpccpassword )
 	hpccresultLimit <- resultLimit
 	connectionlist <- c(hpcchost,hpcctargetCluster,hpccuserid,hpccpassword,hpccresultLimit)
 	connectionlist
